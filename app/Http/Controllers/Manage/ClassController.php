8fe0f8ce82429f3670148909753f42bd674257dd
Anugrah Tribima <73867671+Nugrh@users.noreply.github.com>
2022-01-18 08:02:20 +0700

    Create Class

--- app/Http/Controllers/Manage/ClassController.php
+++ app/Http/Controllers/Manage/ClassController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Manage;
 
+use App\Clas;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -9,10 +10,41 @@ class ClassController extends Controller
 {
     // index
     public function index(){
-        return view('manage.kelas.index');
+        $classes = Clas::latest()->paginate(6);
+
+        return view('manage.kelas.index', compact('classes'));
     }
 
     public function create(){
         return view('manage.kelas.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $this->validate($request, [
+            'name'      => 'required',
+            'walas'     => 'required',
+            'jurusan'   => 'required',
+            'jumlah'    => 'required',
+        ]);
+
+        $class = Clas::create([
+            'name'      => $request->name,
+            'walas'     => $request->walas,
+            'jurusan'   => $request->jurusan,
+            'jumlah'    => $request->jumlah,
+        ]);
+
+        return redirect()->back();
+    }
+
+    public function edit($id)
+    {
+
+        $class = Clas::all()->find($id);
+//        $class = Clas::findOrFail(1);
+
+        return view('manage.kelas.edit', compact('class'));
     }
 }
